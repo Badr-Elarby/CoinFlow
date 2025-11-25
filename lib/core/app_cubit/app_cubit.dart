94d88c2b759cbc2eae0759826353e4c34b316919
mohand804 <mohandabstar@gmail.com>
2025-11-25 04:21:02 +0200

    add AppCubit handling theme & connectivity + Hive setup

---
+++ lib/core/app_cubit/app_cubit.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:team_7/core/app_cubit/app_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:team_7/core/helpers/shared_pref_helper.dart';
+
+class AppCubit extends Cubit<AppState> {
+  AppCubit() : super(AppInitial()) {
+    _loadTheme();
+    checkConnection();
+  }
+
+  StreamSubscription? _subscription;
+
+  void _loadTheme() {
+    final saved = SharedPrefHelper.getString('theme');
+    ThemeMode themeMode = ThemeMode.system;
+
+    if (saved == 'light') {
+      themeMode = ThemeMode.light;
+    } else if (saved == 'dark') {
+      themeMode = ThemeMode.dark;
+    }
+
+    emit(AppInitial(themeMode: themeMode, hasInternet: state.hasInternet));
+  }
+
+  Future<void> toggleTheme() async {
+    final currentTheme = state.themeMode;
+    final newMode = currentTheme == ThemeMode.dark
+        ? ThemeMode.light
+        : ThemeMode.dark;
+
+    emit(AppUpdated(themeMode: newMode, hasInternet: state.hasInternet));
+
+    await SharedPrefHelper.setData('theme', newMode.name);
+  }
+
+  void checkConnection() {
+    _subscription = Connectivity().onConnectivityChanged.listen((
+      List<ConnectivityResult> result,
+    ) {
+      final hasInternet = result.any(
+        (e) => e == ConnectivityResult.wifi || e == ConnectivityResult.mobile,
+      );
+
+      emit(AppUpdated(themeMode: state.themeMode, hasInternet: hasInternet));
+    });
+  }
+
+  @override
+  Future<void> close() async {
+    _subscription?.cancel();
+    return super.close();
+  }
+}
