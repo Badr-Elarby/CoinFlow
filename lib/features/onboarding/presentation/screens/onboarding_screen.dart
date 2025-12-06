e98ea9b582e4fa4d3b74b37661d70f3e4dd16dee
Badr-Elarby <badrshahd291@gmail.com>
2025-12-06 20:14:07 +0200

    refactor the onboarding screens

--- lib/features/onboarding/presentation/screens/onboarding_screen.dart
+++ lib/features/onboarding/presentation/screens/onboarding_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:team_7/core/responsive/size_extensions.dart';
-import 'package:team_7/features/onboarding/presentation/screens/onboarding_page_1.dart';
-import 'package:team_7/features/onboarding/presentation/screens/onboarding_page_2.dart';
-import 'package:team_7/features/onboarding/presentation/screens/onboarding_page_3.dart';
-import 'package:team_7/features/onboarding/presentation/screens/onboarding_page_4.dart';
+import 'package:team_7/features/onboarding/domain/models/onboarding_model.dart';
+import 'package:team_7/features/onboarding/presentation/widgets/onboarding_action_buttons.dart';
+import 'package:team_7/features/onboarding/presentation/widgets/onboarding_content.dart';
 import 'package:team_7/features/onboarding/presentation/widgets/onboarding_navigation.dart';
 
 class OnboardingScreen extends StatefulWidget {
@@ -17,11 +16,24 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   final PageController _pageController = PageController();
   int _currentPage = 0;
 
-  final List<Widget> _pages = const [
-    OnboardingPage1(),
-    OnboardingPage2(),
-    OnboardingPage3(),
-    OnboardingPage4(),
+  final List<OnboardingModel> _pages = const [
+    OnboardingModel(
+      image: 'assets/images/onboarding_1.png',
+      title: 'Welcome To ',
+      highlight: 'Crypto X',
+    ),
+    OnboardingModel(
+      image: 'assets/images/onboarding_2.png',
+      title: 'Transaction Security',
+    ),
+    OnboardingModel(
+      image: 'assets/images/onboarding_3.png',
+      title: 'Fast And Reliable Market\nUpdated',
+    ),
+    OnboardingModel(
+      image: 'assets/images/onboarding_4.png',
+      title: 'Get Started Now!',
+    ),
   ];
 
   void _onNext() {
@@ -35,6 +47,28 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
 
   void _onSkip() {
     _pageController.jumpToPage(_pages.length - 1);
+  }
+
+  Widget _buildPage(int index) {
+    final page = _pages[index];
+    final isLastPage = index == _pages.length - 1;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        OnboardingContent(
+          imagePath: page.image,
+          title: page.title,
+          highlightedText: page.highlight,
+          imageHeight: isLastPage ? context.hp(35) : null,
+          imageWidth: isLastPage ? context.wp(70) : null,
+        ),
+        if (isLastPage) ...[
+          SizedBox(height: context.hp(8)),
+          const OnboardingActionButtons(),
+        ],
+      ],
+    );
   }
 
   @override
@@ -74,14 +108,15 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             ),
 
             Expanded(
-              child: PageView(
+              child: PageView.builder(
                 controller: _pageController,
                 onPageChanged: (index) {
                   setState(() {
                     _currentPage = index;
                   });
                 },
-                children: _pages,
+                itemCount: _pages.length,
+                itemBuilder: (context, index) => _buildPage(index),
               ),
             ),
 
